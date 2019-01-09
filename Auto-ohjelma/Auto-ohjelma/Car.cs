355fed6c6852064f2c8e36dcd3fce2515157bc1e
iidalaukkanen <iida.laukkanen@hotmail.com>
2019-01-09 14:14:02 +0200

    Car program v0.9

---
+++ Auto-ohjelma/Auto-ohjelma/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auto_ohjelma
+{
+    class Car
+    {
+        public string brand;
+        public int speed;
+
+        public Car(string brand, int speed)
+        {
+            this.brand = brand;
+            this.speed = speed;
+        }
+
+        public string AskData()
+        {
+
+        }
+        
+        public string ShowInfo()
+        {
+
+        }
+    }
+}
---
+++ Auto-ohjelma/Auto-ohjelma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Auto_ohjelma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.AskData();
+
+            Car newCar = new Car();
+            newCar.AskData();
+
+            car.ShowInfo();
+            Console.WriteLine("\n_______________________\n");
+            newCar.ShowInfo();
+            Console.WriteLine("\n_______________________\n");
+        }
+    }
+}
