871a5b911f7ebf70a9dd112c3c3e04a6b131ef5d
iidalaukkanen <iida.laukkanen@hotmail.com>
2019-01-09 10:26:27 +0200

    Book program v1.0

---
+++ Kirjaohjelma/Kirjaohjelma/Book.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kirjaohjelma
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public string id;
+        public decimal price;
+
+        public Book(string title, string author, string id, decimal price)
+        {
+            this.title = title;
+            this.author = author;
+            this.id = id;
+            this.price = price;
+        }
+
+        public string PrintInfo()
+        {
+            return $"Nimi: {this.title}\n" +
+                   $"Kirjoittaja: {this.author}\n" +
+                   $"Id: {this.id}\n" +
+                   $"Hinta: {this.price}\n";
+            
+        }
+
+        public string CompareBook(Book book)
+        {
+            string text = "";
+            
+            if (this.price > book.price)
+            {
+                text = $"{this.title} on kalliimpi kuin {book.title}.";
+            }
+            else if (book.price > this.price)
+            {
+                text = $"{book.title} on kalliimpi kuin {this.title}.";
+            }
+            else
+            {
+                text = $"{this.title} ja {book.title} ovat saman hintaisia.";
+            }
+
+            return text;
+        }
+    }
+}
---
+++ Kirjaohjelma/Kirjaohjelma/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Kirjaohjelma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjat: ");
+            Book newBook = new Book("Aapinen", "Mikael Agricola", "00001", 100500.50M);
+            Console.WriteLine(newBook.PrintInfo());
+
+            Book newBook2 = new Book("Peppi Pitkätossu", "Astrid Lindgren", "00002", 18.95M);
+            Console.WriteLine(newBook2.PrintInfo());
+
+            Console.WriteLine(newBook.CompareBook(newBook2)); 
+        }      
+    }
+}
