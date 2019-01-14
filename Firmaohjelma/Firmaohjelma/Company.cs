5dfbd841dc293f8c2156cad6edea6ab9f700546b
iidalaukkanen <iida.laukkanen@hotmail.com>
2019-01-14 11:21:22 +0200

    Company program 0.1

---
+++ Firmaohjelma/Firmaohjelma/Company.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Firmaohjelma
+{
+    class Company
+    {
+        public string title;
+        public string address;
+        public int phone;
+        public int outcome;
+        public int expense;
+
+        public Company()
+        {
+            this.title = "";
+            this.address = "";
+            this.phone = 0;
+            this.outcome = 0;
+            this.expense = 0;
+        }
+
+        public Company(string title, string address, int phone, int outcome, int expense)
+        {
+            this.title = title;
+            this.address = address;
+            this.phone = phone;
+            this.outcome = outcome;
+            this.expense = expense;
+        }
+    }
+}
