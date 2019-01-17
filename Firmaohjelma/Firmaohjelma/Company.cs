209593fc63b12ada2a5ec16fc0c0a9e026aa5e88
iidalaukkanen <iida.laukkanen@hotmail.com>
2019-01-17 13:48:33 +0200

    Company program v1.0

--- Firmaohjelma/Firmaohjelma/Company.cs
+++ Firmaohjelma/Firmaohjelma/Company.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Globalization;
 
 namespace Firmaohjelma
 {
@@ -8,26 +9,61 @@ namespace Firmaohjelma
     {
         public string title;
         public string address;
-        public int phone;
-        public int outcome;
+        public string phone;
+        public int income;
         public int expense;
 
         public Company()
         {
-            this.title = "";
-            this.address = "";
-            this.phone = 0;
-            this.outcome = 0;
+            this.title = string.Empty;
+            this.address = string.Empty;
+            this.phone = string.Empty;
+            this.income = 0;
             this.expense = 0;
         }
 
-        public Company(string title, string address, int phone, int outcome, int expense)
+        public Company(string title, string address, string phone, int income, int expense)
         {
             this.title = title;
             this.address = address;
             this.phone = phone;
-            this.outcome = outcome;
+            this.income = income;
             this.expense = expense;
+        }
+
+        public Company(Company previousCompany)
+        {
+            title = previousCompany.title;
+            address = previousCompany.address;
+            phone = previousCompany.phone;
+            income = previousCompany.income;
+            expense = previousCompany.expense;
+        }
+
+        public string Profit()
+        {
+            string s = string.Empty;
+            decimal profit = (this.income - this.expense) / this.expense;
+
+            if (profit < 1)
+                s = "Voi Jeesus yritä edes...";
+
+            else if (profit <= 2)
+                s = "Nyt vähän tsemppiä!";
+
+            else if (profit <= 3)
+                s = "Jatka samaa rataa.";
+
+            else
+                s = "Hienosti menee!";
+
+            return $"Voittoprosentti: {profit.ToString("p")}. {s}";
+        }
+
+        public override string ToString()
+        {
+            return "Firma:\t" + this.title + "\nOsoite:\t" + this.address + "\nPuh.:\t" + this.phone + "\nTulot:\t" + this.income.ToString("c", CultureInfo.CurrentCulture) + "\nMenot:\t" + this.expense.ToString("c");
+
         }
     }
 }
--- Firmaohjelma/Firmaohjelma/Program.cs
+++ Firmaohjelma/Firmaohjelma/Program.cs
@@ -6,7 +6,18 @@ namespace Firmaohjelma
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("FIRMAN TIEDOT");
+            Console.WriteLine("________________________");
+            Company company = new Company("Akin virma",
+                                          "Päälliköntie 1",
+                                          "0503395919",
+                                           1000500,
+                                           100500);
+
+            Company enterprise = new Company(company);
+            Console.WriteLine(company.ToString());
+            Console.WriteLine(company.Profit());
         }
     }
 }
