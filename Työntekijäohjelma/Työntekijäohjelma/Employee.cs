b7825471ea7022a3eac0977ec0836570cbb87755
iidalaukkanen <iida.laukkanen@hotmail.com>
2019-01-18 11:20:10 +0200

    employee task v0.5

---
+++ Työntekijäohjelma/Työntekijäohjelma/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Työntekijäohjelma
+{
+    class Employee
+    {
+        public string name;
+        public int id;
+        public string position;
+        public string salary;
+        private static int counter = 0;
+
+        public Employee(string name, int id, string position, string salary)
+        {
+            this.name = name;
+            this.id = counter++;
+            this.position = position;
+            this.salary = salary;
+        }
+
+        public string PrintEmployeeInfo()
+        {
+            return $"Työntekijä:\t {this.name} \nId:\t\t {this.id} \nTehtävä:\t {this.position} \nPalkka:\t\t {this.salary}";
+        }
+
+        public void CompareSalary()
+        {
+
+        }
+    }
+}
---
+++ Työntekijäohjelma/Työntekijäohjelma/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Työntekijäohjelma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Täsä röi_akit:\n" +
+            "___________________________________");
+            Employee[] employees = new Employee[3];
+            employees[0] = new Employee("akin ego", 1, "päällikkö", "100500 luonnossa");
+            employees[1] = new Employee("aki", 2, "röi_ukko", "kalia");
+            employees[2] = new Employee("tyhäm aki", 3, "tossutettava", "kolome markkaa");
+
+            foreach(Employee item in employees)
+            {
+                Console.WriteLine($"{item.PrintEmployeeInfo()}\n" +
+                    $"___________________________________");
+            }
+        }
+    }
+}
