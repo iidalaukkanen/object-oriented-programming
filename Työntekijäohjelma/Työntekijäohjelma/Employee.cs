bf325d37d3fe8f9015cbfa283fcd2e0353b2ed58
iidalaukkanen <iida.laukkanen@hotmail.com>
2019-01-22 11:13:23 +0200

    Employee program v1.0

--- Työntekijäohjelma/Työntekijäohjelma/Employee.cs
+++ Työntekijäohjelma/Työntekijäohjelma/Employee.cs
@@ -10,10 +10,10 @@ namespace Työntekijäohjelma
         public string name;
         public int id;
         public string position;
-        public string salary;
-        private static int counter = 0;
+        public double salary;
+        private static int counter = 1;
 
-        public Employee(string name, int id, string position, string salary)
+        public Employee(string name, string position, double salary)
         {
             this.name = name;
             this.id = counter++;
@@ -26,9 +26,28 @@ namespace Työntekijäohjelma
             return $"Työntekijä:\t {this.name} \nId:\t\t {this.id} \nTehtävä:\t {this.position} \nPalkka:\t\t {this.salary}";
         }
 
-        public void CompareSalary()
+        public void CompareSalary(Employee x, Employee y)
         {
+            if(this.salary > x.salary)
+              Console.WriteLine($"{this.name}n palkka {this.salary} on suurempi kuin {x.name}n palkka {x.salary}.");
+            else if(x.salary > this.salary)
+                Console.WriteLine($"{x.name}n palkka {x.salary} on suurempi kuin {this.name}n palkka {this.salary}.");
+            else
+                Console.WriteLine($"{x.name}n ja {this.name}n palkat ovat yhtä suuria.");
 
+            if (this.salary > y.salary)
+                Console.WriteLine($"{this.name}n palkka {this.salary} on suurempi kuin {y.name}n palkka {y.salary}.");
+            else if (y.salary > this.salary)
+                Console.WriteLine($"{y.name}n palkka {y.salary} on suurempi kuin {this.name}n palkka {this.salary}.");
+            else
+                Console.WriteLine($"{y.name}n ja {this.name}n palkat ovat yhtä suuria.");
+
+            if (y.salary > x.salary)
+                Console.WriteLine($"{y.name}n palkka {y.salary} on suurempi kuin {x.name}n palkka {x.salary}.");
+            else if (x.salary > y.salary)
+                Console.WriteLine($"{x.name}n palkka {x.salary} on suurempi kuin {y.name}n palkka {y.salary}.");
+            else
+                Console.WriteLine($"{x.name}n ja {y.name}n palkat ovat yhtä suuria.");
         }
     }
 }
--- Työntekijäohjelma/Työntekijäohjelma/Program.cs
+++ Työntekijäohjelma/Työntekijäohjelma/Program.cs
@@ -6,18 +6,20 @@ namespace Työntekijäohjelma
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Täsä röi_akit:\n" +
+            Console.WriteLine("Täsä työakit:\n" +
             "___________________________________");
             Employee[] employees = new Employee[3];
-            employees[0] = new Employee("akin ego", 1, "päällikkö", "100500 luonnossa");
-            employees[1] = new Employee("aki", 2, "röi_ukko", "kalia");
-            employees[2] = new Employee("tyhäm aki", 3, "tossutettava", "kolome markkaa");
+            employees[0] = new Employee("Akin ego", "päällikkö", 100500);
+            employees[1] = new Employee("Aki", "tyä_ukko", 5000);
+            employees[2] = new Employee("tyhäm Aki", "orja", 3);
 
             foreach(Employee item in employees)
             {
                 Console.WriteLine($"{item.PrintEmployeeInfo()}\n" +
                     $"___________________________________");
             }
+
+            employees[0].CompareSalary(employees[1], employees[2]);
         }
     }
 }
